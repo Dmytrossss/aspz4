0752e8c0fc097c853821e6f3ff17587c7abe7e03
Kyrare <142355469+Dmytrossss@users.noreply.github.com>
2025-06-20 08:18:21 +0300

    Create task4_4_good.c

---
+++ task4_4_good.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    void *ptr = NULL;
+    int counter = 0;
+    
+    while(counter < 3) {
+        if(!ptr) {
+            ptr = malloc(256);
+            printf("Виділено пам'ять: %p\n", ptr);
+        }
+        
+        int *data = (int*)ptr;
+        *data = counter;
+        printf("Значення: %d\n", *data);
+        
+        counter++;
+    }
+    
+    free(ptr);
+    return 0;
+}
